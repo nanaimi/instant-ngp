d263cac09fcf30fb87f7af4e5d6525a3927c67c8
Nasib Naimi <28812908+nanaimi@users.noreply.github.com>
2023-06-26 15:51:55 +0200

    benchmarking script

---
+++ scripts/benchmarkINGP.py
@@ -0,0 +1,154 @@
+#!/usr/bin/env python3
+
+import argparse
+import os
+import commentjson as json
+
+import numpy as np
+
+import shutil
+import time
+
+from common import *
+from scenes import *
+
+from tqdm import tqdm
+
+import wandb
+
+import pyngp as ngp # noqa
+
+
+def parse_args():
+	parser = argparse.ArgumentParser(description="Run instant neural graphics primitives with additional configuration & output options")
+
+	parser.add_argument("files", nargs="*", help="Files to be loaded. Can be a scene, network config, snapshot, camera path, or a combination of those.")
+
+	parser.add_argument("--scene", "--training_data", default="", help="The scene to load. Can be the scene's name or a full path to the training data. Can be NeRF dataset, a *.obj/*.stl mesh for training a SDF, an image, or a *.nvdb volume.")
+	parser.add_argument("--network", default="", help="Path to the network config. Uses the scene's default if unspecified.")
+
+	parser.add_argument("--load_snapshot", "--snapshot", default="", help="Load this snapshot before training. recommended extension: .ingp/.msgpack")
+	parser.add_argument("--save_snapshot", default="", help="Save this snapshot after training. recommended extension: .ingp/.msgpack")
+
+	parser.add_argument("--screenshot_frames", nargs="*", help="Which frame(s) to take screenshots of.")
+	parser.add_argument("--screenshot_dir", default="", help="Which directory to output screenshots to.")
+	parser.add_argument("--screenshot_spp", type=int, default=16, help="Number of samples per pixel in screenshots.")
+
+	parser.add_argument("--width", "--screenshot_w", type=int, default=0, help="Resolution width of GUI and screenshots.")
+	parser.add_argument("--height", "--screenshot_h", type=int, default=0, help="Resolution height of GUI and screenshots.")
+
+	parser.add_argument("--train", action="store_true", help="If the GUI is enabled, controls whether training starts immediately.")
+	parser.add_argument("--n_steps", type=int, default=-1, help="Number of steps to train for before quitting.")
+
+
+	return parser.parse_args()
+
+def get_scene(scene):
+	for scenes in [scenes_sdf, scenes_nerf, scenes_image, scenes_volume]:
+		if scene in scenes:
+			return scenes[scene]
+	return None
+
+if __name__ == "__main__":
+	args = parse_args()
+
+	testbed = ngp.Testbed()
+	testbed.root_dir = ROOT_DIR
+
+	wandb.init(
+		project="instantNGP-4-Compression",
+        name="test",
+		# track hyperparameters and run metadata
+		config=None
+	)
+
+	for file in args.files:
+		scene_info = get_scene(file)
+		if scene_info:
+			file = os.path.join(scene_info["data_dir"], scene_info["dataset"])
+		testbed.load_file(file)
+
+	if args.scene:
+		scene_info = get_scene(args.scene)
+		if scene_info is not None:
+			args.scene = os.path.join(scene_info["data_dir"], scene_info["dataset"])
+			if not args.network and "network" in scene_info:
+				args.network = scene_info["network"]
+
+		testbed.load_training_data(args.scene)
+
+	if args.load_snapshot:
+		scene_info = get_scene(args.load_snapshot)
+		if scene_info is not None:
+			args.load_snapshot = default_snapshot_filename(scene_info)
+		testbed.load_snapshot(args.load_snapshot)
+	elif args.network:
+		testbed.reload_network_from_file(args.network)
+
+	if testbed.mode == ngp.TestbedMode.Sdf:
+		testbed.tonemap_curve = ngp.TonemapCurve.ACES
+
+	testbed.shall_train = args.train
+
+	testbed.nerf.render_with_lens_distortion = True
+
+	network_stem = os.path.splitext(os.path.basename(args.network))[0] if args.network else "base"
+	if testbed.mode == ngp.TestbedMode.Sdf:
+		setup_colored_sdf(testbed, args.scene)
+
+	old_training_step = 0
+	n_steps = args.n_steps
+
+	# If we loaded a snapshot, didn't specify a number of steps, _and_ didn't open a GUI,
+	# don't train by default and instead assume that the goal is to render screenshots,
+	# compute PSNR, or render a video.
+	if n_steps < 0 and (not args.load_snapshot or args.gui):
+		n_steps = 35000
+
+	tqdm_last_update = 0
+	if n_steps > 0:
+		with tqdm(desc="Training", total=n_steps, unit="steps") as t:
+			while testbed.frame():
+				if testbed.want_repl():
+					repl(testbed)
+				# What will happen when training is done?
+				if testbed.training_step >= n_steps:
+					break
+
+        
+				# Update progress bar
+				if testbed.training_step < old_training_step or old_training_step == 0:
+					old_training_step = 0
+					t.reset()
+
+				now = time.monotonic()
+				if now - tqdm_last_update > 0.1:
+					t.update(testbed.training_step - old_training_step)
+					t.set_postfix(loss=testbed.loss)
+					old_training_step = testbed.training_step
+					tqdm_last_update = now
+                
+                psnr = np.log(testbed.loss) * 10.0
+
+                images=None
+                # images = wandb.Image(
+                #     image_array, 
+                #     caption="Top: Output, Bottom: Input"
+                # )
+
+                ## WandB logging
+                wandb.log({"loss": testbed.loss, "training_step": testbed.training_step, "psnr": psnr, "examples": images})
+
+	if args.save_snapshot:
+		testbed.save_snapshot(args.save_snapshot, False)
+
+	
+	if args.screenshot_dir:
+		outname = os.path.join(args.screenshot_dir, args.scene + "_" + network_stem)
+		print(f"Rendering {outname}.png")
+		image = testbed.render(args.width or 1920, args.height or 1080, args.screenshot_spp, True)
+		if os.path.dirname(outname) != "":
+			os.makedirs(os.path.dirname(outname), exist_ok=True)
+		write_image(outname + ".png", image)
+
+
