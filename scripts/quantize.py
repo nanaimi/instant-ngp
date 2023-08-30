3f0cdb4699d759d6cc2863908c4526430833a741
Nasib Naimi <28812908+nanaimi@users.noreply.github.com>
2023-08-30 16:22:34 +0200

    add zero point rounding option to snapshot quant

--- scripts/quantize.py
+++ scripts/quantize.py
@@ -6,7 +6,7 @@ import numpy as np
 
 from utils import compress_and_serialize_ingp, deserialize_ingp  # noqa
 
-T_co = TypeVar('T_co', bound=np.generic, covariant=True)
+T_co = TypeVar("T_co", bound=np.generic, covariant=True)
 
 Vector = np.ndarray[Tuple[int], np.dtype[T_co]]
 Matrix = np.ndarray[Tuple[int, int], np.dtype[T_co]]
@@ -58,7 +58,9 @@ def dequantize_uniform_asymmetric(
     return x_dq
 
 
-def quantize_snapshot(snapshot_path: str, save_dir: str, bits: int = 8) -> None:
+def quantize_snapshot(
+    snapshot_path: str, save_dir: str, bits: int = 8, zero_point_rounding: bool = False
+) -> None:
     """Quantize snapshot and save it to save_dir.
 
     Args:
@@ -73,7 +75,9 @@ def quantize_snapshot(snapshot_path: str, save_dir: str, bits: int = 8) -> None:
     )
     params_array = np.frombuffer(cp_snapshot["snapshot"]["params_binary"], dtype=dtype)
 
-    quant_tuple = quantization_uniform_asymmetric(params_array, bits=bits)
+    quant_tuple = quantization_uniform_asymmetric(
+        params_array, bits=bits, zero_point_rounding=zero_point_rounding
+    )  # noqa
     deq_weights = dequantize_uniform_asymmetric(*quant_tuple)  # noqa
     cp_snapshot["snapshot"]["params_binary"] = deq_weights.tobytes()
 
